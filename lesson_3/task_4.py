9eded9bbf0b84e70f4e7d041c97a1589e48c32b8
lobanovjkee <lobanovjkee@gmail.com>
2020-12-09 10:32:28 +0300

    4 задание

--- lesson_3/task_4.py
+++ lesson_3/task_4.py
@@ -2,23 +2,34 @@
 import random
 
 MIN_VALUE = 0
-MAX_VALUE = 5
-QUANTITY = 100
+MAX_VALUE = 10
+QUANTITY = 1000
+m = 0
+n = 0
 
 array = [random.randint(MIN_VALUE, MAX_VALUE) for _ in range(QUANTITY)]
+print(array)
 max_dict = {}
 for i in array:
     if i in max_dict:
         max_dict[i] += 1
     else:
         max_dict[i] = 1
-# print(max_dict)
-m = 0
-n = 0
+
 for i in max_dict:
-    print(max_dict[i], end='|')
     if max_dict[i] >= m:
         m = max_dict[i]
         n = i
 
-print(f'\n{n} - {m}')
+del max_dict[n]
+temp = [[n, m]]
+
+for i in max_dict:
+    if max_dict[i] == m:
+        temp.append([i, m])
+
+for i in temp:
+    if 2 <= i[1] % 10 <= 4 and (i[1] % 100 < 10 or i[1] % 100 > 20):
+        print(f'Чаще всего встречаются число {i[0]}, {i[1]} раза')
+    else:
+        print(f'Чаще всего встречаются число {i[0]}, {i[1]} раз')
